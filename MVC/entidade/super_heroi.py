78d850532ed858267a1fc40576cf9c75b2414463
Beilfuss <matheusdicimb@gmail.com>
2022-01-27 15:37:36 -0300

    Criação da entidade "super_heroi"

---
+++ MVC/entidade/super_heroi.py
@@ -0,0 +1,15 @@
+from senciente import Senciente
+
+class SuperHeroi(Senciente):
+    def __init__(self, nome: str, poder: Poder, fraqueza: str, empresa: str, local_moradia: str, alterego: str):
+        super().__init__(nome, poder, fraqueza, empresa, local_moradia)
+        self.__alterego = alterego
+
+    @property
+    def alterego(self):
+        return alterego
+
+    @alterego.setter
+    def alterego(self, alterego):
+        self.__alterego = alterego
+
