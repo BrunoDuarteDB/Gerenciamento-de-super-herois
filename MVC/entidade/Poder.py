9135cf4cf00a1dae140e2b0c4412be7c41e172a1
BrunoDuarteDB <brunodborja4@gmail.com>
2022-01-29 20:46:47 -0300

    Criação da entidade "Poder"

---
+++ MVC/entidade/Poder.py
@@ -0,0 +1,85 @@
+class Poder:
+    def __init__(self, velocidade: int, forca: int, poder_magico: int, resistencia: int, inteligencia: int, artes_marciais: int,
+                 fator_cura: int, expertise:int, controle_natureza: int):
+        self.__velocidade= velocidade
+        self.__forca=forca
+        self.__poder_magico=poder_magico
+        self.__resistencia=resistencia
+        self.__inteligencia=inteligencia
+        self.__artes_marciais=artes_marciais
+        self.__fator_cura=fator_cura
+        self.__expertise=expertise
+        self.__controle_natureza=controle_natureza
+
+    @property
+    def velocidade(self):
+        return self.__velocidade
+
+    @property
+    def forca(self):
+        return self.__forca
+
+    @property
+    def poder_magico(self):
+        return self.__poder_magico
+
+    @property
+    def resistencia(self):
+        return self.__resistencia
+
+    @property
+    def inteligencia(self):
+        return self.__inteligencia
+
+    @property
+    def artes_marciais(self):
+        return self.__artes_marciais
+
+    @property
+    def fator_cura(self):
+        return self.__fator_cura
+
+    @property
+    def expertise(self):
+        return self.__expertise
+
+    @property
+    def controle_natureza(self):
+        return self.__controle_natureza
+
+    @velocidade.setter
+    def velocidade(self, velocidade: str):
+        self.__velocidade = velocidade
+
+    @forca.setter
+    def forca(self, forca: str):
+        self.__forca = forca
+
+    @poder_magico.setter
+    def poder_magico(self, poder_magico: str):
+        self.__poder_magico = poder_magico
+
+    @resistencia.setter
+    def resistencia(self, resistencia: str):
+        self.__resistencia = resistencia
+
+    @inteligencia.setter
+    def inteligencia(self, inteligencia: str):
+        self.__inteligencia = inteligencia
+
+    @artes_marciais.setter
+    def artes_marciais(self, artes_marciais: str):
+        self.__artes_marciais = artes_marciais
+
+    @fator_cura.setter
+    def fator_cura(self, fator_cura: str):
+        self.__fator_cura = fator_cura
+
+    @expertise.setter
+    def expertise(self, expertise: str):
+        self.__expertise = expertise
+
+    @controle_natureza.setter
+    def controle_natureza(self, controle_natureza: str):
+        self.__controle_natureza = controle_natureza
+
