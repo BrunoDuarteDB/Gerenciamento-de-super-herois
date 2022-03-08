d3fadc2827d1a7d81d0e1eb2e6f1ee3000ec03a9
Beilfuss <matheusdicimb@gmail.com>
2022-03-08 14:46:52 -0300

    Troca de "buttons" por "submits"

--- MVC/limite/tela_cliente_gui.py
+++ MVC/limite/tela_cliente_gui.py
@@ -3,23 +3,25 @@ import PySimpleGUI as sg
 
 class TelaClienteGUI():
 
-    def __init__(self, controlador_sistema):
-        self.__controlador_sistema = controlador_sistema
+    def __init__(self, controlador_cliente):
+        self.__controlador_cliente = controlador_cliente
         self.__window = None
         self.init_components()
 
     def init_components(self):
+
         layout = [
             [sg.Text('Clientes:')],
-            [sg.Listbox(values=('Listbox 1', 'Listbox 2', 'Listbox 3'), size=(30, 3), key='lb_itens')],
-            [sg.Button('Novo Cliente'), sg.Button('Alterar'), sg.Button('Excluir'), sg.Button('Retornar'),
-             sg.Button('Incluir em Missão')]
+            [sg.Listbox(values=(clientes), size=(30, 3), key='lb_itens')],
+            [sg.Submit(button_text='Novo Cliente'), sg.Submit(button_text='Alterar'), sg.Submit(button_text='Excluir'),
+             sg.Submit(button_text='Retornar'), sg.Submit(button_text='Incluir em Missão')]
         ]
 
         self.__window = sg.Window('TelaCliente', default_element_size=(40, 1)).Layout(layout)
 
     def open(self):
         button, values = self.__window.Read()
+        print(button)
         return button
 
     def close(self):
