46bd38ca724fbf843b0517bdabe60c2e8d137f78
BrunoDuarteDB <brunodborja4@gmail.com>
2022-03-09 22:34:27 -0300

    Modificação tela_cliente_gui

--- MVC/limite/tela_cliente_gui.py
+++ MVC/limite/tela_cliente_gui.py
@@ -12,7 +12,7 @@ class TelaClienteGUI():
 
         layout = [
             [sg.Text('Clientes:')],
-            [sg.Combo(values=dados_clientes, size=(30, 3), key='sb_itens')],
+            [sg.Spin(values=dados_clientes, size=(30, 3), key='sb_itens')],
             [sg.Submit(button_text='Novo Cliente'), sg.Submit(button_text='Alterar'), sg.Submit(button_text='Excluir'),
              # sg.Submit(button_text='Retornar'),
              sg.Submit(button_text='Incluir em Missão')]
